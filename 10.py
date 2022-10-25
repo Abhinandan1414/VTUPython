1eeeda71c7b3e5df606f3ed122da384d1aa6f661
Dr. Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2022-10-25 20:22:54 +0530

    Better Version

---
+++ 10.py
@@ -0,0 +1,16 @@
+from PyPDF2 import PdfFileMerger
+
+#Create and instance of PdfFileMerger() class
+merger = PdfFileMerger()
+
+#Create a list with file names
+pdf_files = ['pdf_files/sample_page1.pdf', 'pdf_files/sample_page2.pdf']
+
+#Iterate over the list of file names
+for pdf_file in pdf_files:
+    #Append PDF files
+    merger.append(pdf_file)
+
+#Write out the merged PDF
+merger.write("merged_2_pages.pdf")
+merger.close()
