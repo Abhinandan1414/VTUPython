ea9f5ad3e5150b239c6c32f6b5e2f2ea080be583
Dr. Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2022-10-25 09:37:30 +0530

    Better Version

---
+++ 6b.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+from zipfile import ZipFile
+from os import path
+from shutil import make_archive
+
+def main_function():
+    archive_name = os.path.expanduser(os.path.join('.', 'myarchive'))
+    root_dir = os.path.expanduser(os.path.join('.', 'directory_tobe_archived'))
+    if not os.path.isdir(root_dir):
+        print('Not a valid directory for archive')
+    make_archive(archive_name, 'zip', root_dir)
+    
+main_function()
