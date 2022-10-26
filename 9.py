c915bb498132a7dfea3858c6ac55f125f8349169
Dr. Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2022-10-26 09:03:48 +0530

    Better Version

--- 9.py
+++ 9.py
@@ -5,18 +5,19 @@ import json
 
 base_url = 'https://xkcd.com/'
 os.makedirs('xkcd', exist_ok=True)
-entered_num = input("Input the comic number ")
+entered_num = input("Input the comic number example 1414: ")
 base_url += entered_num
-print('Downloading image from %s...' % base_url)
+print('Downloading image from location ----' ,base_url)
 res = req.get(base_url)
 soup = bs4.BeautifulSoup(res.text,"html.parser")
 comic_elem = soup.select('#comic img')
 comic_url = 'http:' + comic_elem[0].get('src')
+print("comic_url is: "+comic_url)
 
 res = req.get(comic_url)
 
-imageFile = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
-for chunk in res.iter_content(1):
-	imageFile.write(chunk)
-imageFile.close()
-print('Downloaded the image file')
+image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+for data_chunk in res.iter_content():
+	image_file.write(data_chunk)
+image_file.close()
+print('Downloaded the image file to location',os.path.join('xkcd'),"File name is",os.path.basename(comic_url))
