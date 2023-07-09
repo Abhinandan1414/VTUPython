a8110e712830e8a6f791770658d115bca1a59780
Eng.  Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2023-07-09 09:01:01 +0530

    Update Regression.py

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -9,7 +9,7 @@ def weighted_split(original_list, weight_list,absolute_machine_speeds):
 
         machine_i_test_set.append( original_list[prev_index : next_index] )
         prev_index = next_index
-    for i in len(weight_list):
+    for i in range(len(weight_list)):
         machine_i_test_set_exec_time.append(sum(machine_i_test_set[i])/absolute_machine_speeds[i])
     print(machine_i_test_set)
     print(machine_i_test_set_exec_time)
@@ -31,10 +31,12 @@ def main():
     data_set = [20,30,40,50,60,10,12,13,15,20,24,20]
     absolute_machine_speeds = [1,2,2,1]
     weighted_machine_speeds = [.16,.32,.32,.16]
+    '''
     n = int(input("Enter the machines at disposal"))
     for i in range(n):
         print(identical_machines_total_execution_time(data_set,i+1))
-    
+    '''
+    weighted_split(data_set,weighted_machine_speeds,absolute_machine_speeds)
 main()
     
     
